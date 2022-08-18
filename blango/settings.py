f80a5023b58400c91d257f493959cfb007680ab0
Jansen Simanullang <jansen.simanullang@gmail.com>
2022-08-18 07:29:33 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -194,3 +194,10 @@ class Dev(Configuration):
     # example DJANGO_ADMINS from environment variables
     # DJANGO_ADMINS="Ben Shaw,ben@example.com;Leo Lucio,leo@example.com"
     ADMINS = values.SingleNestedTupleValue()
+
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
